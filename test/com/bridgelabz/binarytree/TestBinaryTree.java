f81f7e559288678035d7a378efb9a57126f475ce
unknown <divyacharaninabha@gmail.com>
2020-10-12 21:14:42 +0530

    Added: functionality to add elements in the tree added

--- test/com/bridgelabz/binarytree/TestBinaryTree.java
+++ test/com/bridgelabz/binarytree/TestBinaryTree.java
@@ -13,8 +13,19 @@ public class TestBinaryTree {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		
 		int size = myBinaryTree.getSize();
-		assertEquals(3, size);
+		assertEquals(13, size);
 	}
 
 }
